f11402dbcb2afefe264b3d045a97647ddd897b35
benedict003 <lliam.benedict@gmail.com>
2026-05-26 07:46:02 +0800

    fix route api

--- app/Http/Controllers/API/ResidentController.php
+++ app/Http/Controllers/API/ResidentController.php
@@ -31,8 +31,32 @@ class ResidentController extends Controller
     {
         $resident = Resident::findOrFail($id);
         $resident->update($request->all());
+        
+        $user = auth()->user();
+        $resident = $user->resident;
 
-        return response()->json($resident);
+        if (!$resident) {
+            return response()->json([
+                'message' => 'Resident not found'
+            ], 404);
+        }
+
+        $resident->update([
+            'first_name' => $request->first_name,
+            'middle_name' => $request->middle_name,
+            'last_name' => $request->last_name,
+            'gender' => $request->gender,
+            'birth_date' => $request->birth_date,
+            'contact_number' => $request->contact_number,
+            'address' => $request->address,
+            'blood_type' => $request->blood_type,
+            'civil_status' => $request->civil_status,
+        ]);
+
+        return response()->json([
+            'message' => 'Profile updated',
+            'resident' => $resident
+        ]);
     }
 
     public function destroy($id)
